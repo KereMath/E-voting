e10a7f390b0d87e7ae19601d27fe7f5b1312a71b
KereMath <keremrgur@gmail.com>
2025-02-21 10:51:28 +0300

    first commit

--- setup.cpp
+++ setup.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <pbc/pbc.h>
+#include <gmp.h>
 
 static const char* BN256_PARAM = R"(
 type f
@@ -20,10 +22,17 @@ TIACParams setupParams() {
     // Pairing'i BN-256 parametreleriyle başlat
     pbc_param_t pbcParams;
     pbc_param_init_set_buf(pbcParams, BN256_PARAM, std::strlen(BN256_PARAM));
-    pairing_init_pbc_param(params.pairing, pbcParams);
+    if (pairing_init_pbc_param(params.pairing, pbcParams) != 0) {
+        std::cerr << "Pairing başlatma hatası!" << std::endl;
+        pbc_param_clear(pbcParams);
+        return params;
+    }
 
     // Grup mertebesini set et
     mpz_init_set(params.prime_order, params.pairing->r);
+    std::cout << "pairing->r: ";
+    mpz_out_str(stdout, 10, params.pairing->r);
+    std::cout << std::endl;
 
     // G1 ve G2 elemanlarını başlat
     element_init_G1(params.g1, params.pairing);
@@ -34,20 +43,25 @@ TIACParams setupParams() {
     std::ifstream file("params.txt");
     if (!file.is_open()) {
         std::cerr << "params.txt dosyasi acilamadi!" << std::endl;
+        pbc_param_clear(pbcParams);
         return params;
     }
 
     std::string line;
-    mpz_t temp;
-    mpz_init(temp);
+    mpz_t temp_x, temp_y;
+    mpz_init(temp_x);
+    mpz_init(temp_y);
 
     // p (Grup mertebesi)
     std::getline(file, line); // "p (Grup mertebesi) ="
     std::getline(file, line);
-    mpz_set_str(temp, line.c_str(), 10);
-    if (mpz_cmp(temp, params.pairing->r) != 0) {
+    mpz_t p;
+    mpz_init(p);
+    mpz_set_str(p, line.c_str(), 10);
+    if (mpz_cmp(p, params.pairing->r) != 0) {
         std::cerr << "params.txt'deki p ile pairing->r eslesmiyor!" << std::endl;
     }
+    mpz_clear(p);
 
     // g1
     std::getline(file, line); // "g1 (G1 uretec) ="
@@ -56,19 +70,15 @@ TIACParams setupParams() {
     size_t comma = g1_str.find(",");
     std::string g1_x = g1_str.substr(0, comma);
     std::string g1_y = g1_str.substr(comma + 1);
-    element_t g1_x_elem, g1_y_elem;
-    element_init_Zr(g1_x_elem, params.pairing);
-    element_init_Zr(g1_y_elem, params.pairing);
-    mpz_set_str(temp, g1_x.c_str(), 10);
-    element_set_mpz(g1_x_elem, temp);
-    mpz_set_str(temp, g1_y.c_str(), 10);
-    element_set_mpz(g1_y_elem, temp);
-    element_set1(params.g1); // Önce bir birim eleman set et
-    element_set_mpz(params.g1, temp); // Sonra koordinatları manuel set et (PBC'nin bir bug'ı olabilir)
-    element_set_si(params.g1->x, mpz_get_si(g1_x_elem->data)); // Doğrudan koordinat set etme
-    element_set_si(params.g1->y, mpz_get_si(g1_y_elem->data));
-    element_clear(g1_x_elem);
-    element_clear(g1_y_elem);
+    mpz_set_str(temp_x, g1_x.c_str(), 10);
+    mpz_set_str(temp_y, g1_y.c_str(), 10);
+    // G1 elemanını set et
+    element_set_mpz(params.g1, temp_x); // x koordinatı
+    element_set_mpz(params.g1, temp_y); // y koordinatı (not: bu bir hata olabilir, aşağıda düzelteceğiz)
+    // Doğru yöntem: element_from_hash veya manuel set
+    std::string g1_combined = g1_x + g1_y;
+    element_from_hash(params.g1, (void*)g1_combined.data(), g1_combined.size());
+    element_printf("Setup - g1 = %B\n", params.g1);
 
     // h1
     std::getline(file, line); // "h1 (G1 ikinci uretec) ="
@@ -77,18 +87,11 @@ TIACParams setupParams() {
     comma = h1_str.find(",");
     std::string h1_x = h1_str.substr(0, comma);
     std::string h1_y = h1_str.substr(comma + 1);
-    element_t h1_x_elem, h1_y_elem;
-    element_init_Zr(h1_x_elem, params.pairing);
-    element_init_Zr(h1_y_elem, params.pairing);
-    mpz_set_str(temp, h1_x.c_str(), 10);
-    element_set_mpz(h1_x_elem, temp);
-    mpz_set_str(temp, h1_y.c_str(), 10);
-    element_set_mpz(h1_y_elem, temp);
-    element_set1(params.h1);
-    element_set_si(params.h1->x, mpz_get_si(h1_x_elem->data));
-    element_set_si(params.h1->y, mpz_get_si(h1_y_elem->data));
-    element_clear(h1_x_elem);
-    element_clear(h1_y_elem);
+    mpz_set_str(temp_x, h1_x.c_str(), 10);
+    mpz_set_str(temp_y, h1_y.c_str(), 10);
+    std::string h1_combined = h1_x + h1_y;
+    element_from_hash(params.h1, (void*)h1_combined.data(), h1_combined.size());
+    element_printf("Setup - h1 = %B\n", params.h1);
 
     // g2
     std::getline(file, line); // "g2 (G2 uretec) ="
@@ -103,30 +106,12 @@ TIACParams setupParams() {
     comma = g2_second.find(",");
     std::string g2_y0 = g2_second.substr(0, comma);
     std::string g2_y1 = g2_second.substr(comma + 1);
-    element_t g2_x0_elem, g2_x1_elem, g2_y0_elem, g2_y1_elem;
-    element_init_Zr(g2_x0_elem, params.pairing);
-    element_init_Zr(g2_x1_elem, params.pairing);
-    element_init_Zr(g2_y0_elem, params.pairing);
-    element_init_Zr(g2_y1_elem, params.pairing);
-    mpz_set_str(temp, g2_x0.c_str(), 10);
-    element_set_mpz(g2_x0_elem, temp);
-    mpz_set_str(temp, g2_x1.c_str(), 10);
-    element_set_mpz(g2_x1_elem, temp);
-    mpz_set_str(temp, g2_y0.c_str(), 10);
-    element_set_mpz(g2_y0_elem, temp);
-    mpz_set_str(temp, g2_y1.c_str(), 10);
-    element_set_mpz(g2_y1_elem, temp);
-    element_set1(params.g2);
-    element_set_si(params.g2->x[0], mpz_get_si(g2_x0_elem->data));
-    element_set_si(params.g2->x[1], mpz_get_si(g2_x1_elem->data));
-    element_set_si(params.g2->y[0], mpz_get_si(g2_y0_elem->data));
-    element_set_si(params.g2->y[1], mpz_get_si(g2_y1_elem->data));
-    element_clear(g2_x0_elem);
-    element_clear(g2_x1_elem);
-    element_clear(g2_y0_elem);
-    element_clear(g2_y1_elem);
+    std::string g2_combined = g2_x0 + g2_x1 + g2_y0 + g2_y1;
+    element_from_hash(params.g2, (void*)g2_combined.data(), g2_combined.size());
+    element_printf("Setup - g2 = %B\n", params.g2);
 
-    mpz_clear(temp);
+    mpz_clear(temp_x);
+    mpz_clear(temp_y);
     file.close();
     pbc_param_clear(pbcParams);
 
@@ -135,6 +120,9 @@ TIACParams setupParams() {
     element_init_GT(gt, params.pairing);
     pairing_apply(gt, params.g1, params.g2, params.pairing);
     element_printf("Setup - e(g1, g2) = %B\n", gt);
+    if (element_is1(gt)) {
+        std::cerr << "Warning: e(g1, g2) birim elemani, pairing hatali olabilir!" << std::endl;
+    }
     element_clear(gt);
 
     return params;
