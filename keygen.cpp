2abdb9d4c10576c84e01eda14c0eb7f9ea5ef34e
KereMath <keremrgur@gmail.com>
2025-02-24 11:21:39 +0300

    first commit

--- keygen.cpp
+++ keygen.cpp
@@ -5,13 +5,11 @@
 #include <iostream>
 
 // Yardımcı: Horner yöntemi ile polinom değerlendirmesi
-// Katsayılar pointer listesi: std::vector<element_s*>
-static void evaluatePoly(std::vector<element_s*> &coeff, int X, TIACParams &params, element_t result) {
+static void evaluatePoly(std::vector<element_t>& coeff, int X, TIACParams &params, element_t result) {
     element_t X_val;
     element_init_Zr(X_val, params.pairing);
     element_set_si(X_val, X);
     
-    // result = coeff[0]
     element_set(result, coeff[0]);
     for (size_t i = 1; i < coeff.size(); i++) {
         element_mul(result, result, X_val);
@@ -23,67 +21,118 @@ static void evaluatePoly(std::vector<element_s*> &coeff, int X, TIACParams &para
 KeyGenOutput keygen(TIACParams &params, int t, int ne) {
     KeyGenOutput output;
     
-    // 1. F ve G polinom katsayıları için 2D vector (EA sayısı x t katsayısı)
-    // Elemanlar element_s* olarak tutulacak.
-    std::vector< std::vector<element_s*> > F_coeffs(ne), G_coeffs(ne);
+    // 1. Initialize F and G polynomial coefficients as 2D vectors of element_t.
+    std::vector< std::vector<element_t> > F_coeffs(ne, std::vector<element_t>(t));
+    std::vector< std::vector<element_t> > G_coeffs(ne, std::vector<element_t>(t));
     for (int i = 0; i < ne; i++) {
-        F_coeffs[i].resize(t);
-        G_coeffs[i].resize(t);
         for (int j = 0; j < t; j++) {
-            F_coeffs[i][j] = new element_s;  
+            // Initialize each coefficient in Zr.
             element_init_Zr(F_coeffs[i][j], params.pairing);
             element_random(F_coeffs[i][j]);
             
-            G_coeffs[i][j] = new element_s;
             element_init_Zr(G_coeffs[i][j], params.pairing);
             element_random(G_coeffs[i][j]);
         }
     }
     
-    // 2. Master secret: msgk = (∏ F_i(0), ∏ G_i(0))
-    element_t prod_F0, prod_G0;
-    element_init_Zr(prod_F0, params.pairing);
-    element_init_Zr(prod_G0, params.pairing);
-    element_set1(prod_F0);
-    element_set1(prod_G0);
+    // 2. Generate commitments Vxij, Vyij, Vyij_prime
+    std::vector<std::vector<element_t>> Vxij(ne, std::vector<element_t>(t));
+    std::vector<std::vector<element_t>> Vyij(ne, std::vector<element_t>(t));
+    std::vector<std::vector<element_t>> Vyij_prime(ne, std::vector<element_t>(t));
     for (int i = 0; i < ne; i++) {
-        element_mul(prod_F0, prod_F0, F_coeffs[i][0]);
-        element_mul(prod_G0, prod_G0, G_coeffs[i][0]);
+        for (int j = 0; j < t; j++) {
+            element_init_G2(Vxij[i][j], params.pairing);
+            element_pow_zn(Vxij[i][j], params.g2, F_coeffs[i][j]);
+            
+            element_init_G2(Vyij[i][j], params.pairing);
+            element_pow_zn(Vyij[i][j], params.g2, G_coeffs[i][j]);
+            
+            element_init_G1(Vyij_prime[i][j], params.pairing);
+            element_pow_zn(Vyij_prime[i][j], params.g1, G_coeffs[i][j]);
+        }
     }
     
-    // 3. Master verification key (mvk)
-    element_t temp;
-    element_init_Zr(temp, params.pairing);
-    element_set1(temp);
-    for (int i = 0; i < ne; i++) {
-        element_t square;
-        element_init_Zr(square, params.pairing);
-        element_mul(square, F_coeffs[i][0], F_coeffs[i][0]);
-        element_mul(temp, temp, square);
-        element_clear(square);
+    // 3. Verify shares using commitments
+    std::vector<bool> complaints(ne, false);
+    for (int i = 1; i <= ne; i++) {
+        for (int l = 0; l < ne; l++) {
+            element_t lhs, rhs, temp, exp;
+            element_init_G2(lhs, params.pairing);
+            element_init_G2(rhs, params.pairing);
+            element_set1(lhs);
+            element_set1(rhs);
+            
+            for (int j = 0; j < t; j++) {
+                element_init_Zr(exp, params.pairing);
+                element_set_si(exp, i * j);
+                
+                element_init_G2(temp, params.pairing);
+                element_pow_zn(temp, Vxij[l][j], exp);
+                element_mul(lhs, lhs, temp);
+                element_clear(temp);
+                
+                element_init_G2(temp, params.pairing);
+                element_pow_zn(temp, Vyij[l][j], exp);
+                element_mul(rhs, rhs, temp);
+                element_clear(temp);
+                
+                element_clear(exp);
+            }
+            
+            element_t Fl_i, Gl_i, g2_Fl_i, g2_Gl_i;
+            element_init_Zr(Fl_i, params.pairing);
+            element_init_Zr(Gl_i, params.pairing);
+            evaluatePoly(F_coeffs[l], i, params, Fl_i);
+            evaluatePoly(G_coeffs[l], i, params, Gl_i);
+            
+            element_init_G2(g2_Fl_i, params.pairing);
+            element_init_G2(g2_Gl_i, params.pairing);
+            element_pow_zn(g2_Fl_i, params.g2, Fl_i);
+            element_pow_zn(g2_Gl_i, params.g2, Gl_i);
+            
+            if (!element_cmp(lhs, g2_Fl_i) || !element_cmp(rhs, g2_Gl_i)) {
+                complaints[l] = true;
+            }
+            
+            element_clear(lhs);
+            element_clear(rhs);
+            element_clear(Fl_i);
+            element_clear(Gl_i);
+            element_clear(g2_Fl_i);
+            element_clear(g2_Gl_i);
+        }
     }
+    
+    // 4. Handle complaints and form subset Q
+    std::vector<int> Q;
+    for (int i = 0; i < ne; i++) {
+        if (!complaints[i]) {
+            Q.push_back(i);
+        }
+    }
+    
+    // 5. Use only authorities in Q to aggregate final sgk, mvk
+    element_t final_sgk1, final_sgk2;
+    element_init_Zr(final_sgk1, params.pairing);
+    element_init_Zr(final_sgk2, params.pairing);
+    element_set1(final_sgk1);
+    element_set1(final_sgk2);
+    
+    for (int i : Q) {
+        element_mul(final_sgk1, final_sgk1, F_coeffs[i][0]);
+        element_mul(final_sgk2, final_sgk2, G_coeffs[i][0]);
+    }
+    
     element_init_G1(output.mvk.alpha2, params.pairing);
-    element_pow_zn(output.mvk.alpha2, params.g1, temp);
+    element_pow_zn(output.mvk.alpha2, params.g1, final_sgk1);
     
-    element_set1(temp);
-    for (int i = 0; i < ne; i++) {
-        element_t square;
-        element_init_Zr(square, params.pairing);
-        element_mul(square, G_coeffs[i][0], G_coeffs[i][0]);
-        element_mul(temp, temp, square);
-        element_clear(square);
-    }
     element_init_G1(output.mvk.beta2, params.pairing);
-    element_pow_zn(output.mvk.beta2, params.g1, temp);
+    element_pow_zn(output.mvk.beta2, params.g1, final_sgk2);
     
-    element_init_G1(output.mvk.beta1, params.pairing);
-    element_pow_zn(output.mvk.beta1, params.g1, prod_G0);
+    element_clear(final_sgk1);
+    element_clear(final_sgk2);
     
-    element_clear(temp);
-    element_clear(prod_F0);
-    element_clear(prod_G0);
-    
-    // 4. Her EA için imza ve doğrulama anahtar paylarının üretimi.
+    // 6. Generate EA keys for each authority in Q
     output.eaKeys.resize(ne);
     for (int i = 1; i <= ne; i++) {
         EAKey ea;
@@ -93,7 +142,7 @@ KeyGenOutput keygen(TIACParams &params, int t, int ne) {
         element_init_Zr(sgk2, params.pairing);
         element_set1(sgk2);
         
-        for (int l = 0; l < ne; l++) {
+        for (int l : Q) {
             element_t valF, valG;
             element_init_Zr(valF, params.pairing);
             element_init_Zr(valG, params.pairing);
@@ -130,13 +179,11 @@ KeyGenOutput keygen(TIACParams &params, int t, int ne) {
         output.eaKeys[i - 1] = ea;
     }
     
-    // 5. Polinom katsayılarını serbest bırakma
+    // 7. Release polynomial coefficients
     for (int i = 0; i < ne; i++) {
         for (int j = 0; j < t; j++) {
             element_clear(F_coeffs[i][j]);
-            delete F_coeffs[i][j];
             element_clear(G_coeffs[i][j]);
-            delete G_coeffs[i][j];
         }
     }
     
