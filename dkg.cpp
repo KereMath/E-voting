aed10eaa9abeeeb373d069dacdd3f99a7883c5c1
KereMath <keremrgur@gmail.com>
2025-02-24 13:55:22 +0300

    first commit

--- dkg.cpp
+++ dkg.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <pbc/pbc.h>
 #include <gmp.h>
-#include <cassert>
-#include <vector>
-#include <string>
 #include <cstring>
-// BN-256 param
-static const char *BN256 = R"(
+
+// -- BN-256 param (type f) --
+static const char* BN256_PARAM = R"(
 type f
 q 186944716490498228592211144210229761989241675946164825413929
 r 186944716490498228592211144210229761989241675946164825526319
@@ -16,8 +14,7 @@ alpha0 147354120310549301445722100263386112552812769040
 alpha1 12707752274141484575335849047546472705710528192
 )";
 
-
-// ---------- Setup --------------
+// ----------------- Setup Yapisi -----------------
 struct TIACParams {
     pairing_t pairing;
     mpz_t prime_order;
@@ -25,12 +22,12 @@ struct TIACParams {
     element_t g2;
 };
 
-static TIACParams setupParams() {
+TIACParams setupParams() {
     TIACParams p;
-    pbc_param_t pp;
-    pbc_param_init_set_buf(pp, BN256, strlen(BN256));
-    pairing_init_pbc_param(p.pairing, pp);
-    pbc_param_clear(pp);
+    pbc_param_t pbc;
+    pbc_param_init_set_buf(pbc, BN256_PARAM, std::strlen(BN256_PARAM));
+    pairing_init_pbc_param(p.pairing, pbc);
+    pbc_param_clear(pbc);
 
     mpz_init_set(p.prime_order, p.pairing->r);
 
@@ -40,44 +37,43 @@ static TIACParams setupParams() {
     element_random(p.g1);
     element_random(p.g2);
 
-    // e(g1,g2) != 1
-    element_t checkGT;
-    element_init_GT(checkGT, p.pairing);
-    pairing_apply(checkGT, p.g1, p.g2, p.pairing);
-    int tries=0;
-    while(element_is1(checkGT) && tries<32){
+    // e(g1,g2) != 1 kontrolü
+    element_t tmpGT;
+    element_init_GT(tmpGT, p.pairing);
+    pairing_apply(tmpGT, p.g1, p.g2, p.pairing);
+    int tries = 0;
+    while(element_is1(tmpGT) && tries<32) {
         element_random(p.g1);
         element_random(p.g2);
-        pairing_apply(checkGT, p.g1, p.g2, p.pairing);
+        pairing_apply(tmpGT, p.g1, p.g2, p.pairing);
         tries++;
     }
-    element_clear(checkGT);
+    element_clear(tmpGT);
 
     return p;
 }
 
-
-// ---------- DKG Key Structures -----------
+// --------------- EAKey / Output Yapilari -----------
 struct EAKey {
-    // sabit terim x0, y0
-    element_t x0;
+    // sabit terimler
+    element_t x0; 
     element_t y0;
 
     // t+1 commitments
-    std::vector<element_t> Vx;
-    std::vector<element_t> Vy;
-    std::vector<element_t> Vyprime;
+    element_t *Vx;      // dizi => g2^( x_{ij} )
+    element_t *Vy;      // dizi => g2^( y_{ij} )
+    element_t *Vyprime; // dizi => g1^( y_{ij} )
 
-    // local share
-    element_t sgk1; // ∑ F_l(i)
-    element_t sgk2; // ∑ G_l(i)
-    element_t vki1; // g2^(sgk1)
-    element_t vki2; // g2^(sgk2)
-    element_t vki3; // g1^(sgk2)
+    // local pay
+    element_t sgk1;
+    element_t sgk2;
+    element_t vki1;
+    element_t vki2;
+    element_t vki3;
 };
 
 struct MasterVK {
-    element_t vk1;
+    element_t vk1; 
     element_t vk2;
     element_t vk3;
 };
@@ -90,18 +86,22 @@ struct MasterSK {
 struct KeyGenOutput {
     MasterVK mvk;
     MasterSK msgk;
-    std::vector<EAKey> eaKeys;
+    EAKey* eaKeys; // dizi [n]
 };
 
-// -------- Evaluate Poly -----------
-static void evaluatePoly(element_s *coeffs, int t, int L, TIACParams &params, element_t outVal)
+// --------------- Evaluate Polynom (mod r) ----------------
+static void evaluatePoly(element_s *coeffs, // katsayilari [t+1]
+                         int t,
+                         int L,
+                         TIACParams &params,
+                         element_t outVal)
 {
-    // F_i(L) = ∑ coeffs[k]*L^k (mod r)
+    // outVal = ∑_{k=0..t} coeffs[k] * L^k (mod r)
     element_set0(outVal);
 
     element_t xVal;
     element_init_Zr(xVal, params.pairing);
-    element_set_si(xVal, L);
+    element_set_si(xVal, L);  // L mod r
 
     element_t power;
     element_init_Zr(power, params.pairing);
@@ -110,7 +110,8 @@ static void evaluatePoly(element_s *coeffs, int t, int L, TIACParams &params, el
     element_t tmp;
     element_init_Zr(tmp, params.pairing);
 
-    for(int k_i=0; k_i<=t; k_i++){
+    for(int k_i = 0; k_i <= t; k_i++){
+        // tmp = coeffs[k_i]*power
         element_mul(tmp, &coeffs[k_i], power);
         element_add(outVal, outVal, tmp);
         element_mul(power, power, xVal);
@@ -121,46 +122,48 @@ static void evaluatePoly(element_s *coeffs, int t, int L, TIACParams &params, el
     element_clear(tmp);
 }
 
-
-// ---------- KeyGen (Pedersen DKG) ---------------
+// --------------- KeyGen (Pedersen's DKG) ----------------
 KeyGenOutput keygen(TIACParams &params, int t, int n) {
     KeyGenOutput out;
-    out.eaKeys.resize(n);
+    // EAKey dizisi
+    out.eaKeys = new EAKey[n];
 
-    // Polinom katsayıları
-    element_s **F = new element_s*[n];
-    element_s **G = new element_s*[n];
+    // Polinom katsayilari F[i], G[i] => pointer dizisi
+    element_s** F = new element_s*[n];
+    element_s** G = new element_s*[n];
 
+    // 1) Rastgele polinom olusturma
     for(int i=0; i<n; i++){
         F[i] = new element_s[t+1];
         G[i] = new element_s[t+1];
         for(int j=0; j<=t; j++){
             element_init_Zr(&F[i][j], params.pairing);
             element_random(&F[i][j]);
-
             element_init_Zr(&G[i][j], params.pairing);
             element_random(&G[i][j]);
         }
     }
 
-    // EA Keys: fill commitments
+    // 2) EAKey: commitments (t+1 dizisi)
     for(int i=0; i<n; i++){
-        out.eaKeys[i].Vx.resize(t+1);
-        out.eaKeys[i].Vy.resize(t+1);
-        out.eaKeys[i].Vyprime.resize(t+1);
+        out.eaKeys[i].Vx      = new element_t[t+1];
+        out.eaKeys[i].Vy      = new element_t[t+1];
+        out.eaKeys[i].Vyprime = new element_t[t+1];
 
+        // init + pow
         for(int j=0; j<=t; j++){
             element_init_G2(out.eaKeys[i].Vx[j], params.pairing);
-            element_init_G2(out.eaKeys[i].Vy[j], params.pairing);
-            element_init_G1(out.eaKeys[i].Vyprime[j], params.pairing);
-
             element_pow_zn(out.eaKeys[i].Vx[j], params.g2, &F[i][j]);
+
+            element_init_G2(out.eaKeys[i].Vy[j], params.pairing);
             element_pow_zn(out.eaKeys[i].Vy[j], params.g2, &G[i][j]);
+
+            element_init_G1(out.eaKeys[i].Vyprime[j], params.pairing);
             element_pow_zn(out.eaKeys[i].Vyprime[j], params.g1, &G[i][j]);
         }
     }
 
-    // sabit terimler
+    // 3) sabit terimler x0,y0
     for(int i=0; i<n; i++){
         element_init_Zr(out.eaKeys[i].x0, params.pairing);
         element_set(out.eaKeys[i].x0, &F[i][0]);
@@ -169,7 +172,7 @@ KeyGenOutput keygen(TIACParams &params, int t, int n) {
         element_set(out.eaKeys[i].y0, &G[i][0]);
     }
 
-    // pay doğrulama
+    // 4) Pay dogrulama
     element_t fVal, gVal;
     element_init_Zr(fVal, params.pairing);
     element_init_Zr(gVal, params.pairing);
@@ -187,18 +190,18 @@ KeyGenOutput keygen(TIACParams &params, int t, int n) {
 
     for(int i=0; i<n; i++){
         for(int L=1; L<=n; L++){
-            evaluatePoly(F[i], t, L, params, fVal);  // F_i(L)
-            evaluatePoly(G[i], t, L, params, gVal);  // G_i(L)
+            // F_i(L), G_i(L)
+            evaluatePoly(F[i], t, L, params, fVal);
+            evaluatePoly(G[i], t, L, params, gVal);
 
-            // g2^F_i(L)
+            // lhs = g2^F_i(L)
             element_pow_zn(lhsG2, params.g2, fVal);
-
-            // RHS = ∏_{j=0..t} [ Vx[i][j] ^( L^j ) ]
+            // rhs = ∏ [ Vx[i][j]^( L^j )]
             element_set1(rhsG2);
             long powInt=1;
             for(int j=0; j<=t; j++){
-                if(j>0) powInt *= L;
-                element_set_si(eExp, powInt);
+                if(j>0) powInt *= L; // normal int
+                element_set_si(eExp, powInt); // eExp in Zr
                 element_t tmpG2;
                 element_init_G2(tmpG2, params.pairing);
                 element_pow_zn(tmpG2, out.eaKeys[i].Vx[j], eExp);
@@ -209,7 +212,7 @@ KeyGenOutput keygen(TIACParams &params, int t, int n) {
                 std::cerr<<"[WARN] F_i("<<L<<") mismatch => i="<<i<<"\n";
             }
 
-            // g2^G_i(L)
+            // lhs = g2^G_i(L)
             element_pow_zn(lhsG2, params.g2, gVal);
             element_set1(rhsG2);
             powInt=1;
@@ -226,7 +229,7 @@ KeyGenOutput keygen(TIACParams &params, int t, int n) {
                 std::cerr<<"[WARN] G_i("<<L<<") mismatch G2 => i="<<i<<"\n";
             }
 
-            // g1^G_i(L)
+            // lhs = g1^G_i(L)
             element_pow_zn(lhsG1, params.g1, gVal);
             element_set1(rhsG1);
             powInt=1;
@@ -253,7 +256,7 @@ KeyGenOutput keygen(TIACParams &params, int t, int n) {
     element_clear(lhsG1);
     element_clear(rhsG1);
 
-    // Master VK
+    // 5) Master VK
     element_init_G2(out.mvk.vk1, params.pairing);
     element_init_G2(out.mvk.vk2, params.pairing);
     element_init_G1(out.mvk.vk3, params.pairing);
@@ -267,18 +270,17 @@ KeyGenOutput keygen(TIACParams &params, int t, int n) {
         element_mul(out.mvk.vk3, out.mvk.vk3, out.eaKeys[i].Vyprime[0]);
     }
 
-    // Master SK
+    // 6) Master SK
     element_init_Zr(out.msgk.sk1, params.pairing);
     element_init_Zr(out.msgk.sk2, params.pairing);
     element_set0(out.msgk.sk1);
     element_set0(out.msgk.sk2);
-
     for(int i=0; i<n; i++){
         element_add(out.msgk.sk1, out.msgk.sk1, &F[i][0]);
         element_add(out.msgk.sk2, out.msgk.sk2, &G[i][0]);
     }
 
-    // local pay
+    // 7) local share
     for(int i=0; i<n; i++){
         element_init_Zr(out.eaKeys[i].sgk1, params.pairing);
         element_init_Zr(out.eaKeys[i].sgk2, params.pairing);
@@ -299,12 +301,13 @@ KeyGenOutput keygen(TIACParams &params, int t, int n) {
         element_init_G2(out.eaKeys[i].vki1, params.pairing);
         element_init_G2(out.eaKeys[i].vki2, params.pairing);
         element_init_G1(out.eaKeys[i].vki3, params.pairing);
+
         element_pow_zn(out.eaKeys[i].vki1, params.g2, out.eaKeys[i].sgk1);
         element_pow_zn(out.eaKeys[i].vki2, params.g2, out.eaKeys[i].sgk2);
         element_pow_zn(out.eaKeys[i].vki3, params.g1, out.eaKeys[i].sgk2);
     }
 
-    // polinom clear
+    // 8) Polinom bellek temizligi
     for(int i=0; i<n; i++){
         for(int j=0; j<=t; j++){
             element_clear(&F[i][j]);
@@ -319,108 +322,95 @@ KeyGenOutput keygen(TIACParams &params, int t, int n) {
     return out;
 }
 
-
-// -------- main -----------
+// --------------- main ---------------
 int main(){
     int n=3, t=2;
+
+    // Setup
     TIACParams p = setupParams();
 
-    // print p
-    char *strp = mpz_get_str(nullptr,10, p.prime_order);
-    std::cout<<"p = "<<strp<<"\n";
-    free(strp);
+    // Test e(g1,g2)
+    element_t tmpGT;
+    element_init_GT(tmpGT, p.pairing);
+    pairing_apply(tmpGT, p.g1, p.g2, p.pairing);
+
+    char* strP = mpz_get_str(nullptr,10, p.prime_order);
+    std::cout<<"p = "<<strP<<"\n"; free(strP);
 
     {
-        char b[4096];
-        element_snprintf(b,sizeof(b),"%B",p.g1);
-        std::cout<<"g1 = "<<b<<"\n";
-        element_snprintf(b,sizeof(b),"%B",p.g2);
-        std::cout<<"g2 = "<<b<<"\n";
+        char buf[2048];
+        element_snprintf(buf,sizeof(buf),"%B",p.g1);
+        std::cout<<"g1 = "<<buf<<"\n";
+        element_snprintf(buf,sizeof(buf),"%B",p.g2);
+        std::cout<<"g2 = "<<buf<<"\n";
+        element_snprintf(buf,sizeof(buf),"%B",tmpGT);
+        std::cout<<"e(g1,g2)= "<<buf<<"\n\n";
     }
+    element_clear(tmpGT);
 
-    element_t check;
-    element_init_GT(check, p.pairing);
-    pairing_apply(check, p.g1, p.g2, p.pairing);
+    // KeyGen
+    KeyGenOutput out = keygen(p,t,n);
+
+    // Print mvk
     {
-        char b[4096];
-        element_snprintf(b,sizeof(b),"%B",check);
-        std::cout<<"e(g1,g2)= "<<b<<"\n\n";
+        char b1[2048], b2[2048], b3[2048];
+        element_snprintf(b1,sizeof(b1),"%B",out.mvk.vk1);
+        element_snprintf(b2,sizeof(b2),"%B",out.mvk.vk2);
+        element_snprintf(b3,sizeof(b3),"%B",out.mvk.vk3);
+        std::cout<<"mvk.vk1= "<<b1<<"\n";
+        std::cout<<"mvk.vk2= "<<b2<<"\n";
+        std::cout<<"mvk.vk3= "<<b3<<"\n\n";
     }
-    element_clear(check);
 
-    KeyGenOutput K = keygen(p,t,n);
-
+    // Print msgk
     {
-        char b[2048];
-        element_snprintf(b,sizeof(b),"%B",K.mvk.vk1);
-        std::cout<<"mvk.vk1= "<<b<<"\n";
-        element_snprintf(b,sizeof(b),"%B",K.mvk.vk2);
-        std::cout<<"mvk.vk2= "<<b<<"\n";
-        element_snprintf(b,sizeof(b),"%B",K.mvk.vk3);
-        std::cout<<"mvk.vk3= "<<b<<"\n\n";
+        char a1[1024], a2[1024];
+        element_snprintf(a1,sizeof(a1),"%B",out.msgk.sk1);
+        element_snprintf(a2,sizeof(a2),"%B",out.msgk.sk2);
+        std::cout<<"msgk.sk1= "<<a1<<"\n";
+        std::cout<<"msgk.sk2= "<<a2<<"\n\n";
     }
 
-    {
-        char b1[1024], b2[1024];
-        element_snprintf(b1,sizeof(b1),"%B",K.msgk.sk1);
-        element_snprintf(b2,sizeof(b2),"%B",K.msgk.sk2);
-        std::cout<<"msgk.sk1= "<<b1<<"\n";
-        std::cout<<"msgk.sk2= "<<b2<<"\n";
-    }
-
+    // Print EA keys
     for(int i=0; i<n; i++){
-        std::cout<<"=== EA #"<<(i+1)<<" ===\n";
+        std::cout<<"=== EA "<<(i+1)<<" ===\n";
         char bx[512], by[512];
-        element_snprintf(bx,sizeof(bx),"%B",K.eaKeys[i].x0);
-        element_snprintf(by,sizeof(by),"%B",K.eaKeys[i].y0);
-        std::cout<<" x0= "<<bx<<"\n y0= "<<by<<"\n";
-        std::cout<<" commitments:\n";
-        for(int j=0; j<=t; j++){
-            char vx[512], vy[512], vyp[512];
-            element_snprintf(vx,sizeof(vx),"%B",K.eaKeys[i].Vx[j]);
-            element_snprintf(vy,sizeof(vy),"%B",K.eaKeys[i].Vy[j]);
-            element_snprintf(vyp,sizeof(vyp),"%B",K.eaKeys[i].Vyprime[j]);
-            std::cout<<"   j="<<j<<" => Vx="<<vx<<", Vy="<<vy<<", Vy'="<<vyp<<"\n";
-        }
-        char s1[512], s2[512], v1[512], v2[512], v3[512];
-        element_snprintf(s1,sizeof(s1),"%B",K.eaKeys[i].sgk1);
-        element_snprintf(s2,sizeof(s2),"%B",K.eaKeys[i].sgk2);
-        element_snprintf(v1,sizeof(v1),"%B",K.eaKeys[i].vki1);
-        element_snprintf(v2,sizeof(v2),"%B",K.eaKeys[i].vki2);
-        element_snprintf(v3,sizeof(v3),"%B",K.eaKeys[i].vki3);
-
-        std::cout<<" sgk1= "<<s1<<"\n sgk2= "<<s2<<"\n";
-        std::cout<<" vki1= "<<v1<<"\n vki2= "<<v2<<"\n vki3= "<<v3<<"\n\n";
+        element_snprintf(bx,sizeof(bx),"%B",out.eaKeys[i].x0);
+        element_snprintf(by,sizeof(by),"%B",out.eaKeys[i].y0);
+        std::cout<<" x0= "<<bx<<"\n y0= "<<by<<"\n\n";
     }
 
-    // bellek clear
-    element_clear(K.mvk.vk1);
-    element_clear(K.mvk.vk2);
-    element_clear(K.mvk.vk3);
-    element_clear(K.msgk.sk1);
-    element_clear(K.msgk.sk2);
+    // Bellek temizle
+    element_clear(out.mvk.vk1);
+    element_clear(out.mvk.vk2);
+    element_clear(out.mvk.vk3);
+    element_clear(out.msgk.sk1);
+    element_clear(out.msgk.sk2);
 
     for(int i=0; i<n; i++){
-        element_clear(K.eaKeys[i].x0);
-        element_clear(K.eaKeys[i].y0);
+        element_clear(out.eaKeys[i].x0);
+        element_clear(out.eaKeys[i].y0);
         for(int j=0; j<=t; j++){
-            element_clear(K.eaKeys[i].Vx[j]);
-            element_clear(K.eaKeys[i].Vy[j]);
-            element_clear(K.eaKeys[i].Vyprime[j]);
+            element_clear(out.eaKeys[i].Vx[j]);
+            element_clear(out.eaKeys[i].Vy[j]);
+            element_clear(out.eaKeys[i].Vyprime[j]);
         }
-        element_clear(K.eaKeys[i].sgk1);
-        element_clear(K.eaKeys[i].sgk2);
-        element_clear(K.eaKeys[i].vki1);
-        element_clear(K.eaKeys[i].vki2);
-        element_clear(K.eaKeys[i].vki3);
-    }
+        delete[] out.eaKeys[i].Vx;
+        delete[] out.eaKeys[i].Vy;
+        delete[] out.eaKeys[i].Vyprime;
 
-    // param clearing
+        element_clear(out.eaKeys[i].sgk1);
+        element_clear(out.eaKeys[i].sgk2);
+        element_clear(out.eaKeys[i].vki1);
+        element_clear(out.eaKeys[i].vki2);
+        element_clear(out.eaKeys[i].vki3);
+    }
+    delete[] out.eaKeys;
+
     element_clear(p.g1);
     element_clear(p.g2);
     mpz_clear(p.prime_order);
     pairing_clear(p.pairing);
 
-    std::cout<<"[INFO] end.\n";
     return 0;
 }
