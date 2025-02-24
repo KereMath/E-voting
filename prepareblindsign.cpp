63d605bf39dcfc19d286423a53388c2a3d720e95
KereMath <keremrgur@gmail.com>
2025-02-24 17:04:15 +0300

    Rename DIDgen to didgen in a case-sensitive way

--- prepareblindsign.cpp
+++ prepareblindsign.cpp
@@ -9,8 +9,7 @@
 /*
   PBC’de:
     element_t = struct element_s[1];
-  Dolayısıyla g1, h, vs. birer "struct element_s" dizisi (uzunluk 1).
-  Dizinin adı (g1, h vb.) -> decay -> (element_s*)
+  Dolayısıyla g1, h, vs. birer "struct element_s" dizisidir.
 */
 
 ////////////////////////////////////
@@ -24,7 +23,6 @@ static void randomZr(element_t zr, TIACParams &params) {
 
 // DID (hex) -> mpz mod p
 static void didStringToMpz(const std::string &didStr, mpz_t rop, const mpz_t p) {
-    // 16 taban: parse
     if(mpz_set_str(rop, didStr.c_str(), 16) != 0) {
         throw std::runtime_error("didStringToMpz: invalid hex string");
     }
@@ -32,9 +30,7 @@ static void didStringToMpz(const std::string &didStr, mpz_t rop, const mpz_t p) 
 }
 
 // G1 elemanını hex string’e çevir 
-// (burada parametre "element_s *" tipinde)
 static std::string elementToStringG1(element_s *elemPtr) {
-    // element_length_in_bytes, element_to_bytes => (element_t) = (element_s *)
     int length = element_length_in_bytes(elemPtr);
     std::vector<unsigned char> buf(length);
     element_to_bytes(buf.data(), elemPtr);
@@ -47,25 +43,19 @@ static std::string elementToStringG1(element_s *elemPtr) {
     return oss.str();
 }
 
-// outG1 = hash(G1)  
+// outG1 = hash(G1)
 static void hashToG1(element_t outG1, TIACParams &params, element_t inElem) {
-    // inElem => "struct element_s[1]"
-    // inElem decays to (element_s *), 
     std::string s = elementToStringG1(inElem); 
-    // PBC: element_from_hash(element_t e, void *data, int len)
     element_from_hash(outG1, s.data(), s.size());
 }
 
 // c = Hash( [elems] ) -> Zr
-// "elems" = vector of "element_s *"
 static void hashToZr(element_t outZr, TIACParams &params, const std::vector<element_s*> &elems) {
     std::ostringstream oss;
     for(auto ePtr : elems) {
-        // ePtr : element_s*
         oss << elementToStringG1(ePtr);
     }
     std::string msg = oss.str();
-
     unsigned char digest[SHA512_DIGEST_LENGTH];
     SHA512((unsigned char*)msg.data(), msg.size(), digest);
 
@@ -73,7 +63,6 @@ static void hashToZr(element_t outZr, TIACParams &params, const std::vector<elem
     mpz_init(tmp);
     mpz_import(tmp, SHA512_DIGEST_LENGTH, 1, 1, 0, 0, digest);
     mpz_mod(tmp, tmp, params.prime_order);
-
     element_set_mpz(outZr, tmp);
     mpz_clear(tmp);
 }
@@ -93,13 +82,10 @@ static KoRProof computeKoR(
     mpz_t o
 ) {
     KoRProof proof;
-
-    // r1, r2, r3 ∈ Zr
     element_t r1, r2, r3;
     element_init_Zr(r1, params.pairing);
     element_init_Zr(r2, params.pairing);
     element_init_Zr(r3, params.pairing);
-
     randomZr(r1, params);
     randomZr(r2, params);
     randomZr(r3, params);
@@ -107,17 +93,12 @@ static KoRProof computeKoR(
     // comi' = g1^r1 * h1^r2
     element_t comi_prime;
     element_init_G1(comi_prime, params.pairing);
-
-    // g1^r1
     element_t g1_r1;
     element_init_G1(g1_r1, params.pairing);
     element_pow_zn(g1_r1, g1, r1);
-
-    // h1^r2
     element_t h1_r2;
     element_init_G1(h1_r2, params.pairing);
     element_pow_zn(h1_r2, h1, r2);
-
     element_mul(comi_prime, g1_r1, h1_r2);
     element_clear(g1_r1);
     element_clear(h1_r2);
@@ -125,54 +106,41 @@ static KoRProof computeKoR(
     // com' = g1^r3 * h^r2
     element_t com_prime;
     element_init_G1(com_prime, params.pairing);
-
     element_t g1_r3;
     element_init_G1(g1_r3, params.pairing);
     element_pow_zn(g1_r3, g1, r3);
-
     element_t h_r2;
     element_init_G1(h_r2, params.pairing);
     element_pow_zn(h_r2, h, r2);
-
     element_mul(com_prime, g1_r3, h_r2);
     element_clear(g1_r3);
     element_clear(h_r2);
 
-    // proof.c, s1, s2, s3 init
     element_init_Zr(proof.c,  params.pairing);
     element_init_Zr(proof.s1, params.pairing);
     element_init_Zr(proof.s2, params.pairing);
     element_init_Zr(proof.s3, params.pairing);
 
-    // Hash(g1, h, h1, com, comi, com_prime, comi_prime)
-    // Hepsi: element_t => "struct element_s[1]"
-    // => Dizinin ismi "g1" decays -> (element_s*) 
-    //   "com_prime" -> (element_s*)
     std::vector<element_s*> toHash;
     toHash.reserve(7);
-    toHash.push_back(g1);         // g1 => element_s*
-    toHash.push_back(h);          // h => element_s*
-    toHash.push_back(h1);         // h1 => element_s*
-    toHash.push_back(com);        // com => element_s*
-    toHash.push_back(com_prime);  // com_prime => element_s*
-    toHash.push_back(comi);       // comi => element_s*
-    toHash.push_back(comi_prime); // comi_prime => element_s*
-
+    toHash.push_back(g1);
+    toHash.push_back(h);
+    toHash.push_back(h1);
+    toHash.push_back(com);
+    toHash.push_back(com_prime);
+    toHash.push_back(comi);
+    toHash.push_back(comi_prime);
     hashToZr(proof.c, params, toHash);
 
-    // c -> mpz
     mpz_t c_mpz;
     mpz_init(c_mpz);
     element_to_mpz(c_mpz, proof.c);
-
-    // r1, r2, r3 -> mpz
     mpz_t r1_mpz, r2_mpz, r3_mpz;
     mpz_inits(r1_mpz, r2_mpz, r3_mpz, NULL);
     element_to_mpz(r1_mpz, r1);
     element_to_mpz(r2_mpz, r2);
     element_to_mpz(r3_mpz, r3);
 
-    // s1 = r1 - c*oi
     mpz_t s1_mpz;
     mpz_init(s1_mpz);
     mpz_mul(s1_mpz, c_mpz, oi);
@@ -180,7 +148,6 @@ static KoRProof computeKoR(
     mpz_mod(s1_mpz, s1_mpz, params.prime_order);
     element_set_mpz(proof.s1, s1_mpz);
 
-    // s2 = r2 - c*did
     mpz_t s2_mpz;
     mpz_init(s2_mpz);
     mpz_mul(s2_mpz, c_mpz, did);
@@ -188,7 +155,6 @@ static KoRProof computeKoR(
     mpz_mod(s2_mpz, s2_mpz, params.prime_order);
     element_set_mpz(proof.s2, s2_mpz);
 
-    // s3 = r3 - c*o
     mpz_t s3_mpz;
     mpz_init(s3_mpz);
     mpz_mul(s3_mpz, c_mpz, o);
@@ -196,7 +162,6 @@ static KoRProof computeKoR(
     mpz_mod(s3_mpz, s3_mpz, params.prime_order);
     element_set_mpz(proof.s3, s3_mpz);
 
-    // temizlik
     mpz_clears(c_mpz, r1_mpz, r2_mpz, r3_mpz, s1_mpz, s2_mpz, s3_mpz, NULL);
     element_clear(r1);
     element_clear(r2);
@@ -212,11 +177,10 @@ static KoRProof computeKoR(
 ////////////////////////////////////
 PrepareBlindSignOutput prepareBlindSign(TIACParams &params, const std::string &didStr) {
     PrepareBlindSignOutput out;
-
     mpz_t oi, o;
     mpz_inits(oi, o, NULL);
 
-    // Rastgele oi, o
+    // Rastgele oi, o hesapla
     {
         element_t tmp;
         element_init_Zr(tmp, params.pairing);
@@ -242,7 +206,6 @@ PrepareBlindSignOutput prepareBlindSign(TIACParams &params, const std::string &d
     {
         element_t g1_oi;
         element_init_G1(g1_oi, params.pairing);
-
         {
             element_t exp;
             element_init_Zr(exp, params.pairing);
@@ -250,10 +213,8 @@ PrepareBlindSignOutput prepareBlindSign(TIACParams &params, const std::string &d
             element_pow_zn(g1_oi, params.g1, exp);
             element_clear(exp);
         }
-
         element_t h1_did;
         element_init_G1(h1_did, params.pairing);
-
         {
             element_t exp;
             element_init_Zr(exp, params.pairing);
@@ -261,7 +222,6 @@ PrepareBlindSignOutput prepareBlindSign(TIACParams &params, const std::string &d
             element_pow_zn(h1_did, params.h1, exp);
             element_clear(exp);
         }
-
         element_mul(out.comi, g1_oi, h1_did);
         element_clear(g1_oi);
         element_clear(h1_did);
@@ -276,7 +236,6 @@ PrepareBlindSignOutput prepareBlindSign(TIACParams &params, const std::string &d
     {
         element_t g1_o;
         element_init_G1(g1_o, params.pairing);
-
         {
             element_t exp;
             element_init_Zr(exp, params.pairing);
@@ -284,10 +243,8 @@ PrepareBlindSignOutput prepareBlindSign(TIACParams &params, const std::string &d
             element_pow_zn(g1_o, params.g1, exp);
             element_clear(exp);
         }
-
         element_t h_did;
         element_init_G1(h_did, params.pairing);
-
         {
             element_t exp;
             element_init_Zr(exp, params.pairing);
@@ -295,7 +252,6 @@ PrepareBlindSignOutput prepareBlindSign(TIACParams &params, const std::string &d
             element_pow_zn(h_did, out.h, exp);
             element_clear(exp);
         }
-
         element_mul(out.com, g1_o, h_did);
         element_clear(g1_o);
         element_clear(h_did);
@@ -313,6 +269,10 @@ PrepareBlindSignOutput prepareBlindSign(TIACParams &params, const std::string &d
         didInt,
         o
     );
+
+    // ÖNEMLİ: "o" alanını da output yapısına kopyalayalım
+    mpz_init(out.o);
+    mpz_set(out.o, o);
 
     mpz_clears(oi, o, didInt, NULL);
     return out;
