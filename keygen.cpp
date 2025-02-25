02c1f5a15bfc9d89c2565f7f2d8150d4fbc978bb
KereMath <keremrgur@gmail.com>
2025-02-25 13:43:14 +0300

    concurrent

--- keygen.cpp
+++ keygen.cpp
@@ -6,8 +6,26 @@
 #include <tbb/parallel_for.h>
 #include <tbb/global_control.h>
 
-// [0, p-1] aralığında rastgele mpz üretir.
-static void random_mpz_modp(mpz_t rop, const mpz_t p) {
+// Simple wrapper class for mpz_t to allow storage in std::vector.
+class MPZWrapper {
+public:
+    mpz_t value;
+    MPZWrapper() { mpz_init(value); }
+    MPZWrapper(const MPZWrapper &other) {
+        mpz_init(value);
+        mpz_set(value, other.value);
+    }
+    MPZWrapper& operator=(const MPZWrapper &other) {
+        if (this != &other) {
+            mpz_set(value, other.value);
+        }
+        return *this;
+    }
+    ~MPZWrapper() { mpz_clear(value); }
+};
+
+// [0, p-1] aralığında rastgele MPZWrapper üretir.
+static void random_mpz_modp(MPZWrapper &rop, const mpz_t p) {
     static std::random_device rd;
     static std::mt19937_64 gen(rd());
     size_t bits = mpz_sizeinbase(p, 2);
@@ -16,27 +34,27 @@ static void random_mpz_modp(mpz_t rop, const mpz_t p) {
     for (size_t i = 0; i < bytes; i++) {
         buf[i] = static_cast<unsigned char>(gen() & 0xFF);
     }
-    mpz_import(rop, bytes, 1, 1, 0, 0, buf.data());
-    mpz_mod(rop, rop, p);
+    mpz_import(rop.value, bytes, 1, 1, 0, 0, buf.data());
+    mpz_mod(rop.value, rop.value, p);
 }
 
-// t adet (t-1) dereceli polinom katsayısı üretir.
-static void randomPolynomial(std::vector<mpz_t> &poly, int t, const mpz_t p) {
+// t adet (t-1) dereceli polinom katsayılarını [0, p-1] rastgele seç (MPZWrapper kullanarak).
+static void randomPolynomial(std::vector<MPZWrapper> &poly, int t, const mpz_t p) {
     poly.resize(t);
     for (int i = 0; i < t; i++) {
-        mpz_init(poly[i]);
         random_mpz_modp(poly[i], p);
     }
 }
 
-// poly(x) = sum_{k=0}^{t-1} [ poly[k] * x^k ] mod p
-static void evalPolynomial(mpz_t result, const std::vector<mpz_t> &poly, int xValue, const mpz_t p) {
+// poly(x) = sum_{k=0}^{t-1} [ poly[k] * x^k ] mod p.
+// poly[k] tipimiz MPZWrapper'dır; mpz_t değerine poly[k].value ile erişiyoruz.
+static void evalPolynomial(mpz_t result, const std::vector<MPZWrapper> &poly, int xValue, const mpz_t p) {
     mpz_set_ui(result, 0);
     mpz_t term, xPow;
     mpz_init(term);
     mpz_init_set_ui(xPow, 1); // x^0 = 1
     for (size_t k = 0; k < poly.size(); k++) {
-        mpz_mul(term, poly[k], xPow);
+        mpz_mul(term, poly[k].value, xPow);
         mpz_add(result, result, term);
         mpz_mod(result, result, p);
         mpz_mul_ui(xPow, xPow, xValue);
@@ -59,8 +77,8 @@ KeyGenOutput keygen(TIACParams &params, int t, int ne) {
     KeyGenOutput keyOut;
     keyOut.eaKeys.resize(ne);
 
-    // Polinom katsayılarını üret.
-    std::vector<mpz_t> vPoly, wPoly;
+    // Polinom katsayılarını üret: vPoly ve wPoly.
+    std::vector<MPZWrapper> vPoly, wPoly;
     randomPolynomial(vPoly, t, params.prime_order);
     randomPolynomial(wPoly, t, params.prime_order);
 
@@ -88,25 +106,21 @@ KeyGenOutput keygen(TIACParams &params, int t, int ne) {
     element_pow_zn(keyOut.mvk.beta2, params.g2, expY);
     element_pow_zn(keyOut.mvk.beta1, params.g1, expY);
 
-    // Paralel kontrol: EA otoriteleri için hesaplamaları paralelleştiriyoruz.
-    // Her EA'nın hesaplamaları birbirinden bağımsızdır.
+    // Paralel hesaplama: EA otoriteleri için döngü.
     unsigned int numThreads = std::thread::hardware_concurrency();
     if (numThreads == 0) numThreads = 4;
     tbb::global_control gc(tbb::global_control::max_allowed_parallelism, numThreads);
-
     tbb::parallel_for(0, ne, [&](int m_index) {
         int m = m_index + 1;
-        // m'in katsayılarını hesapla.
         mpz_t xm, ym;
         mpz_init(xm);
         mpz_init(ym);
+        // v(m), w(m) hesaplanıyor.
         evalPolynomial(xm, vPoly, m, params.prime_order);
         evalPolynomial(ym, wPoly, m, params.prime_order);
-
-        // EA anahtarı sgk: (xm, ym)
+        // sgk = (xm, ym)
         element_set_mpz(keyOut.eaKeys[m_index].sgk1, xm);
         element_set_mpz(keyOut.eaKeys[m_index].sgk2, ym);
-
         // vkm hesaplamaları:
         element_t expXm, expYm;
         element_init_Zr(expXm, params.pairing);
@@ -118,16 +132,11 @@ KeyGenOutput keygen(TIACParams &params, int t, int ne) {
         element_pow_zn(keyOut.eaKeys[m_index].vkm3, params.g1, expYm);
         element_clear(expXm);
         element_clear(expYm);
-
         mpz_clear(xm);
         mpz_clear(ym);
     });
 
-    // Temizlik: Polinom katsayıları.
-    for (int i = 0; i < t; i++) {
-        mpz_clear(vPoly[i]);
-        mpz_clear(wPoly[i]);
-    }
+    // Polinom katsayılarını temizlemek için, MPZWrapper'lar otomatik temizlenecek.
     mpz_clear(x);
     mpz_clear(y);
     element_clear(expX);
