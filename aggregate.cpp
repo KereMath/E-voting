4d7bb31715b34e4cf54b441f6af12e8a834863e7
unknown <kerem.gur@metu.edu.tr>
2025-03-04 15:16:13 +0300

    see

--- aggregate.cpp
+++ aggregate.cpp
@@ -12,9 +12,7 @@ std::string elementToStringG1(element_t elem); // Dışarıdan sağlanmış fonk
 // Yardımcı fonksiyon: const element_t'yi (yani element_s* ) non-const element_s*'ye çevirir.
 static element_s* toNonConst(element_t in) {
     return const_cast<element_s*>(in);
-}
-
-AggregateSignature aggregateSign(
+}AggregateSignature aggregateSign(
     TIACParams &params,
     const std::vector<UnblindSignature> &partialSigs,
     MasterVerKey &mvk,
@@ -25,7 +23,7 @@ AggregateSignature aggregateSign(
     
     // (1) h: Tüm parçalarda h aynı; ilk partial imzadan alınır.
     element_init_G1(aggSig.h, params.pairing);
-    element_set(aggSig.h, toNonConst(partialSigs[0].h));
+    element_set(aggSig.h, const_cast<element_s*>(&partialSigs[0].h[0]));
     debugStream << "Aggregate h set from first partial signature.\n";
     
     // (2) s: Başlangıçta grup identity elemanı (s = 1) olarak ayarlanır.
@@ -35,10 +33,10 @@ AggregateSignature aggregateSign(
     
     // (3) Her partial imza parçasının s_m değeri ile aggregate s'yi çarpıyoruz.
     for (size_t i = 0; i < partialSigs.size(); i++) {
-        std::string part = elementToStringG1(toNonConst(partialSigs[i].s_m));
+        std::string part = elementToStringG1(const_cast<element_s*>(&partialSigs[i].s_m[0]));
         debugStream << "Multiplying with partial signature " << (i+1)
                     << " s_m = " << part << "\n";
-        element_mul(aggSig.s, aggSig.s, toNonConst(partialSigs[i].s_m));
+        element_mul(aggSig.s, aggSig.s, const_cast<element_s*>(&partialSigs[i].s_m[0]));
     }
     debugStream << "Aggregate s computed = " << elementToStringG1(aggSig.s) << "\n";
     
