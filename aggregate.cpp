ede5302d77debb1b71824216aa34533cd70b9c88
unknown <kerem.gur@metu.edu.tr>
2025-03-04 15:12:56 +0300

    see

--- aggregate.cpp
+++ aggregate.cpp
@@ -21,7 +21,7 @@ AggregateSignature aggregateSign(
     // (1) h: Tüm parçalarda h aynı; ilk partial imzadan alınır.
     element_init_G1(aggSig.h, params.pairing);
     // partialSigs[0].h'yi const_cast ile kopyalıyoruz (uyarı almamak için).
-    element_set(aggSig.h, const_cast<element_t>(partialSigs[0].h));
+    element_set(aggSig.h, &partialSigs[0].h[0]);
     debugStream << "Aggregate h set from first partial signature.\n";
     
     // (2) s: Başlangıçta grup identity elemanı (s = 1) olarak ayarlanır.
@@ -31,11 +31,11 @@ AggregateSignature aggregateSign(
     
     // (3) Her partial imza parçasının s_m değeri ile aggregate s'yi çarpıyoruz.
     for (size_t i = 0; i < partialSigs.size(); i++) {
-        std::string part = elementToStringG1(const_cast<element_t>(partialSigs[i].s_m));
+        std::string part = elementToStringG1(&partialSigs[i].s_m[0]);
         debugStream << "Multiplying with partial signature " << (i+1)
                     << " s_m = " << part << "\n";
-        element_mul(aggSig.s, aggSig.s, const_cast<element_t>(partialSigs[i].s_m));
-    }
+                    element_mul(aggSig.s, aggSig.s, &partialSigs[i].s_m[0]);
+                }
     debugStream << "Aggregate s computed = " << elementToStringG1(aggSig.s) << "\n";
     
     // (4) Pairing kontrolü kaldırıldı (unblind aşamasında zaten yapıldı).
