25b62757d2fd924605b131d9abf9c8d200145d38
unknown <kerem.gur@metu.edu.tr>
2025-03-04 16:35:16 +0300

    see

--- aggregate.cpp
+++ aggregate.cpp
@@ -3,7 +3,8 @@
 #include <sstream>
 #include <iomanip>
 
-// elementToStringG1 tanımını dışarıdan alıyoruz (const parametre alacak şekilde)
+// Externally defined: elementToStringG1, 
+// artık const element_t parametre alacak şekilde tanımlanmıştır.
 extern std::string elementToStringG1(const element_t elem);
 
 AggregateSignature aggregateSign(TIACParams &params,
@@ -15,22 +16,27 @@ AggregateSignature aggregateSign(TIACParams &params,
     }
     
     AggregateSignature aggSig;
+    // Initialize aggregate signature components:
     element_init_G1(aggSig.h, params.pairing);
-    element_set(aggSig.h, partialSigs[0].h);
-    
     element_init_G1(aggSig.s, params.pairing);
-    element_set(aggSig.s, partialSigs[0].s_m);
+
+    // Set h using the first partial signature. PartialSigs[].h is const, so cast it away.
+    element_set(aggSig.h, const_cast<element_t>(partialSigs[0].h));
     
-    // Her partial imza için s_m'leri çarparız.
+    // Set s initially to the first partial signature's s_m.
+    element_set(aggSig.s, const_cast<element_t>(partialSigs[0].s_m));
+    
+    // For each remaining partial signature, multiply s_m'leri.
     for (size_t i = 1; i < partialSigs.size(); i++) {
-        element_mul(aggSig.s, aggSig.s, partialSigs[i].s_m);
+        // Cast partialSigs[i].s_m from const to non-const for element_mul.
+        element_mul(aggSig.s, aggSig.s, const_cast<element_t>(partialSigs[i].s_m));
     }
     
-    // Debug bilgisi: her partial s_m'nin string gösterimini birleştirir.
+    // Build debug info by concatenating the string representations of each partial s_m.
     std::ostringstream oss;
     oss << "Aggregated s (product of s_m's): ";
     for (size_t i = 0; i < partialSigs.size(); i++) {
-        oss << elementToStringG1(partialSigs[i].s_m) << " ";
+        oss << elementToStringG1(const_cast<element_t>(partialSigs[i].s_m)) << " ";
     }
     aggSig.debug_info = oss.str();
     
